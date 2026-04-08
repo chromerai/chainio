ff4ef1b5e7fb93c8cffcd9798d7885cb0fd1aaf9
chromerai <kaustubhmishra983@gamil.com>
2026-04-09 02:02:37 +0530

    09: sidebar layout

---
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,18 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+    params: Promise<{
+        credentialId: String;
+    }>
+}
+
+const Page = async ({ params }: PageProps) => {
+    await requireAuth()
+    
+    const { credentialId }  = await params;
+    return (
+        <p>Credential Id: {credentialId}</p>
+    );
+}
+
+export default Page;
---
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -0,0 +1,10 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+const Page = async () => {
+    await requireAuth()
+    return (
+        <p>Workflow Page</p>
+    );
+}
+
+export default Page;
