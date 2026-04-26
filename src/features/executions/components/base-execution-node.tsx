2a4d953bc947111e2c748c4a5b6aa0fc8da76e1d
chromerai <kaustubhmishra983@gamil.com>
2026-04-26 17:00:09 +0530

    17: editor state

--- src/features/executions/components/base-execution-node.tsx
+++ src/features/executions/components/base-execution-node.tsx
@@ -1,24 +1,24 @@
 "use client"
 
-import { type NodeProps, Position } from "@xyflow/react";
+import { type NodeProps, Position, useReactFlow } from "@xyflow/react";
 import type { LucideIcon } from "lucide-react";
 import Image from "next/image";
 import { memo, type ReactNode, useCallback} from "react";
 import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
 import { BaseHandle } from "@/components/react-flow/base-handle";
 import { WorkflowNode } from "@/components/workflow-node";
+import { type NodeStatus, NodeStatusIndicator } from "@/components/react-flow/node-status-indicator";
 
 interface BaseExecutionNodeProps extends NodeProps {
     icon: LucideIcon | string;
     name: string;
     description?: string;
     children?: ReactNode;
-    //status?: NodeStatus;
+    status?: NodeStatus;
     onSettings?: () => void;
     onDoubleClick?: () => void; 
 };
 
-const handleDelete = () => {};
 
 export const BaseExecutionNode = memo(
     (
@@ -28,10 +28,25 @@ export const BaseExecutionNode = memo(
             name,
             description,
             children,
+            status = "initial",
             onSettings,
             onDoubleClick,
         }: BaseExecutionNodeProps 
     ) => {
+
+        const {setNodes, setEdges} = useReactFlow();
+        
+        const handleDelete = () => {
+            setNodes((curNodes) => {
+                const updatedNodes = curNodes.filter((node) => node.id !== id);
+                return updatedNodes;
+            });
+
+            setEdges((curEdges) => {
+                const updatedEdges = curEdges.filter((edge) => edge.source !== id && edge.target !== id);
+                return updatedEdges;
+            });
+        };
         return (
             <WorkflowNode
                 name={name}
@@ -39,28 +54,33 @@ export const BaseExecutionNode = memo(
                 onDelete={handleDelete}
                 onSettings={onSettings}
             >
-                <BaseNode onDoubleClick={onDoubleClick}>
-                    <BaseNodeContent>
-                        {
-                            typeof Icon === "string" ? (
-                                <Image src={Icon} alt={name} width={16} height={16} />
-                            ): (
-                                <Icon className="size-4 text-muted-foreground" />
-                            )
-                        }
-                        {children}
-                        <BaseHandle
-                            id="target-1"
-                            type="target"
-                            position={Position.Left}
-                        />
-                        <BaseHandle
-                            id="source-1"
-                            type="source"
-                            position={Position.Right}
-                        />
-                    </BaseNodeContent>
-                </BaseNode>
+                <NodeStatusIndicator
+                    status={status}
+                    variant="border"
+                >
+                    <BaseNode status={status} onDoubleClick={onDoubleClick}>
+                        <BaseNodeContent>
+                            {
+                                typeof Icon === "string" ? (
+                                    <Image src={Icon} alt={name} width={16} height={16} />
+                                ): (
+                                    <Icon className="size-4 text-muted-foreground" />
+                                )
+                            }
+                            {children}
+                            <BaseHandle
+                                id="target-1"
+                                type="target"
+                                position={Position.Left}
+                            />
+                            <BaseHandle
+                                id="source-1"
+                                type="source"
+                                position={Position.Right}
+                            />
+                        </BaseNodeContent>
+                    </BaseNode>
+                </NodeStatusIndicator>
             </WorkflowNode>
         )
     },
